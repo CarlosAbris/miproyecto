6b3a1181e26fb4127bac36de1286343d8774d6c0
Carlos Abrisqueta <iescierva.carlos@gmail.com>
2018-10-19 19:00:05 +0000

    Carrito 4.3 Patrón Singleton

--- public/tema11/ejemplo5.php
+++ public/tema11/ejemplo5.php
@@ -24,7 +24,7 @@ session_start();
 	$p2 = new Producto("Cereales de chocolate", 5.99);
 	$p3 = new Producto("Servilletas 20x20", 1.2);
 
-	$carrito = new Carrito();
+	$carrito = Carrito::getCarrito();
 	$carrito->meter($p1);
 	$carrito->meter($p2);
 	$carrito->meter($p3);
