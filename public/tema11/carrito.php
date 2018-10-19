1e378c2be02e17b5f0c4b0c6828d3a71f5bd541a
Carlos Abrisqueta <iescierva.carlos@gmail.com>
2018-10-19 17:57:03 +0000

    Carrito 3.0

--- public/tema11/carrito.php
+++ public/tema11/carrito.php
@@ -1,12 +1,22 @@
 <?php
 
+interface iEnCarrito
+{
+	public function mostrar();
+	public function precio();
+	public function precioIva();
+	public function masUnidad();
+	public function menosUnidad();
+	public function permiteUnidades();
+}
+
 class Carrito
 {
 	private $productos = [];
 
-	public function meter($producto)
+	public function meter(iEnCarrito $elemento)
 	{
-		$this->productos[] = $producto;
+		$this->productos[] = $elemento;
 	}
 
 	public function mostrar()
@@ -16,10 +26,21 @@ class Carrito
 
 		echo '<div class="carrito">';
 
-		foreach($this->productos as $producto) {
+		foreach($this->productos as $key => $producto) {
+
 			echo '<article class="lineacarrito">';
 			echo $producto->mostrar();
+
+			$enlaceMasUnidad = "?accion=masunidad&indice=$key";
+			$enlaceMenosUnidad = "?accion=menosunidad&indice=$key";
+			$enlaceEliminar = "?accion=eliminar&indice=$key";
+
+			if ($producto->permiteUnidades()) {
+				echo "<a href=\"$enlaceMenosUnidad\">-</a> / <a href=\"$enlaceMasUnidad\">+</a>";
+			}
+			echo "<a href=\"$enlaceEliminar\" class=\"enlaceeliminar\">Eliminar</a>";
 			echo '</article>';
+
 			$total += $producto->precio();
 			$totalIva += $producto->precioIva();
 		}
@@ -47,7 +68,7 @@ class Carrito
 }
 
 
-class Producto
+class Producto implements iEnCarrito
 {
 	private $nombre;
 	private $precio;
@@ -88,5 +109,123 @@ class Producto
 		}
 	}
 
+	public function permiteUnidades()
+	{
+		return true;
+	}
+
 }
+
+
+class Descuento implements iEnCarrito
+{
+	private $motivo;
+	private $descuento;
+
+	public function __construct($motivo, $descuento)
+	{
+		$this->motivo = $motivo;
+		$this->descuento = $descuento;
+	}
+
+	public function mostrar()
+	{
+		return '<p class="descuento">' . $this->motivo . ' ' . $this->descuento . ' &euro;</p>';
+	}
+
+	public function precio()
+	{
+		return - $this->descuento;
+	}
+
+	public function precioIva()
+	{
+		return $this->precio();
+	}
+
+	public function masUnidad()
+	{
+		return false;
+	}
+
+	public function menosUnidad()
+	{
+		return false;
+	}
+
+	public function permiteUnidades()
+	{
+		return false;
+	}
+}
+
+
+class Pack implements iEnCarrito
+{
+	private $productosPack;
+	private $cantidad = 1;
+
+	public function __construct($arrayProductos)
+	{
+		$this->productosPack = $arrayProductos;
+	}
+
+	public function mostrar()
+	{
+		$salida = '<div class="pack">';
+
+		foreach ($this->productosPack as $key => $value) {
+			$salida .= $value->mostrar();
+			$salida .= '<br />';
+		}
+
+		$salida .= '</div>';
+
+		return $salida;
+	}
+
+	public function permiteUnidades()
+	{
+		return true;
+	}
+
+	public function precio()
+	{
+		$total = 0;
+
+		foreach ($this->productosPack as $producto) {
+			$total += $producto->precio();
+		}
+
+		return $total * $this->cantidad;
+	}
+
+	public function precioIva()
+	{
+		$total = 0;
+
+		foreach ($this->productosPack as $producto) {
+			$total += $producto->precioIva();
+		}
+
+		return $total * $this->cantidad;
+	}
+
+	public function masUnidad($unidades = 1)
+	{
+		$this->cantidad += $unidades;
+	}
+
+	public function menosUnidad()
+	{
+		if ($this->cantidad > 0) {
+			$this->cantidad--;
+		}
+	}
+}
+
+
+
+
+
 
